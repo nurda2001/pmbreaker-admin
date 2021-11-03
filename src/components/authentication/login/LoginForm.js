c6b07c900892995b706b8609ef3cb431a1709b7b
Nurdaulet <m.n.m.nurdaulet@gmail.com>
2021-11-04 05:29:39 +0600

    First Commit

---
+++ src/components/authentication/login/LoginForm.js
@@ -0,0 +1,101 @@
+import * as Yup from 'yup';
+import { useState } from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { useFormik, Form, FormikProvider } from 'formik';
+import { Icon } from '@iconify/react';
+import eyeFill from '@iconify/icons-eva/eye-fill';
+import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
+// material
+import {
+  Link,
+  Stack,
+  Checkbox,
+  TextField,
+  IconButton,
+  InputAdornment,
+  FormControlLabel
+} from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+
+// ----------------------------------------------------------------------
+
+export default function LoginForm() {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    login: Yup.string().required('Логин не может быть пустым'),
+    password: Yup.string().required('Пароль не может быть пустым')
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      login: '',
+      password: '',
+      remember: true
+    },
+    validationSchema: LoginSchema,
+    onSubmit: () => {
+      navigate('/dashboard', { replace: true });
+    }
+  });
+
+  const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
+
+  const handleShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="login"
+            label="Логин"
+            {...getFieldProps('login')}
+            error={Boolean(touched.login && errors.login)}
+            helperText={touched.login && errors.login}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Пароль"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleShowPassword} edge="end">
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+        </Stack>
+
+        <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{ my: 2 }}>
+          <Link component={RouterLink} variant="subtitle2" to="#">
+            Забыли пароль?
+          </Link>
+        </Stack>
+
+        <LoadingButton
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          loading={isSubmitting}
+        >
+          Login
+        </LoadingButton>
+      </Form>
+    </FormikProvider>
+  );
+}
---
+++ src/pages/Page404.js
@@ -0,0 +1,54 @@
+import { motion } from 'framer-motion';
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import { styled } from '@mui/material/styles';
+import { Box, Button, Typography, Container } from '@mui/material';
+// components
+import { MotionContainer, varBounceIn } from '../components/animate';
+import Page from '../components/Page';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(Page)(({ theme }) => ({
+  display: 'flex',
+  minHeight: '100%',
+  alignItems: 'center',
+  paddingTop: theme.spacing(15),
+  paddingBottom: theme.spacing(10)
+}));
+
+// ----------------------------------------------------------------------
+
+export default function Page404() {
+  return (
+    <RootStyle title="404 Page Not Found | Minimal-UI">
+      <Container>
+        <MotionContainer initial="initial" open>
+          <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
+            <motion.div variants={varBounceIn}>
+              <Typography variant="h3" paragraph>
+                Страница была не найдена!
+              </Typography>
+            </motion.div>
+            <Typography sx={{ color: 'text.secondary' }}>
+              Простите, мы не смогли найти вашу страницу. Может ошибка в URL? Внимательно проверяйте
+              ваш запрос
+            </Typography>
+
+            <motion.div variants={varBounceIn}>
+              <Box
+                component="img"
+                src="/static/illustrations/illustration_404.svg"
+                sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
+              />
+            </motion.div>
+
+            <Button to="/" size="large" variant="contained" component={RouterLink}>
+              Назад
+            </Button>
+          </Box>
+        </MotionContainer>
+      </Container>
+    </RootStyle>
+  );
+}
